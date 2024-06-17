9c6c2b70923146c13e8dc9f933a78d7a4e1ed2ce
kamalisham98 <kamalisham98@gmail.com>
2024-06-17 16:26:17 +0700

    chore: add localtem

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module ApiGameOnline
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.default_locale = :id
+
+    config.active_record.schema_format = :sql
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
