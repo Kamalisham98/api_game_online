2acdc3e34a11f9584f82280719897e38a562d7af
kamalisham98 <kamalisham98@gmail.com>
2024-06-23 12:40:29 +0700

    fix: credential for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    # 'http://localhost:4000',
-    # 'http://127.0.0.1:4000',
-    # 'http://yourwebsite.production.app'
+    origins 'mallsgame.com' if Rails.env.production?
+    origins 'localhost:3001',
+            '127.0.0.1:3001'
 
     resource '*', headers: :any,
-                  methods: %i[get post put patch delete options head]
-    # credentials: true
+                  methods: %i[get post put patch delete options head],
+                  credentials: true,
+                  max_age: 86_400
   end
 end
